f4a55dfb2dc591a9affac37c2c820a7e405c781e
Henrik Matthiesen <henrikmatthiesen11@gmail.com>
2023-02-28 18:13:57 -0600

    code cleaned

--- 1Dclustering.cpp
+++ 1Dclustering.cpp
@@ -2,19 +2,18 @@
 //implementation of clustering in 1 dimension
 //we apply this to Graph after dimension reduction via heat flow
 
-//wait until heat in initial point is cooled off to (factor)*(equilibrium amount)
-
 #include "clustering.h"
 
 
 //attempts 1D clustering via kernel density estimator
 std::unordered_map<double,int>* oneDimensionalClustering(std::vector<double>& data, double significance, double bandWidth, double minClusterSize){
     //try to locate clusters by locating jumps in density
-    //approximate density via windows of size bandWidth
-    //if maxDensity>=significance*minDensity we label cluster
+    //approximates (up to normalization) density of one dimensional distribution using step functions of width bandWidth
+    //identifies clusters if points of large density are separated by points of small density
+    //if maxDensity>=significance*minDensity we assign to a cluster
+    //separation between different clusters if density drops low aain
    
     int currLabel=0;
- 
     std::unordered_map<double, int>* clusterLabel = new std::unordered_map<double, int>;
 
     std::sort(data.begin(),data.end());
@@ -31,20 +30,25 @@ std::unordered_map<double,int>* oneDimensionalClustering(std::vector<double>& da
     int steps=0;
 
     //compute maxDensity in windows of size bandWidth
-    while(centerOfWindow>-1){
-        if(steps%2==0){
+    while(centerOfWindow>-1)
+    {
+        if(steps%2==0)
+        {
             dataCenter=data[centerOfWindow];
         }
-        else{
+        else
+        {
             dataCenter=(data[centerOfWindow+1]+data[centerOfWindow])/2;
         }
 
         //compute density in window of size bandWidth centered at centerOfWindow
-        while(data[endOfWindow]-dataCenter>bandWidth){
+        while(data[endOfWindow]-dataCenter>bandWidth)
+        {
             endOfWindow--;
         }
         //now window ends at endOfWindow (inclusive)
-        while(startOfWindow>0 && dataCenter-data[startOfWindow-1]<=bandWidth){
+        while(startOfWindow>0 && dataCenter-data[startOfWindow-1]<=bandWidth)
+        {
             startOfWindow--;
         }
         //now window starts at startOfWindow (inclusive)
@@ -53,7 +57,8 @@ std::unordered_map<double,int>* oneDimensionalClustering(std::vector<double>& da
         if(minDensity==-1){minDensity=currDensity;}
         minDensity=std::min(minDensity,currDensity);
         //update of iteration parameters
-        if(steps%2==0){
+        if(steps%2==0)
+        {
             centerOfWindow--;
         }
         steps++;
@@ -72,28 +77,35 @@ std::unordered_map<double,int>* oneDimensionalClustering(std::vector<double>& da
     //keep track if point with high density in between
     bool cluster=false;
     int count=0;
-    while(centerOfWindow>-1){
-        if(steps%2==0){
+    while(centerOfWindow>-1)
+    {
+        if(steps%2==0)
+        {
             dataCenter=data[centerOfWindow];
         }
-        else{
+        else
+        {
             dataCenter=(data[centerOfWindow+1]+data[centerOfWindow])/2;
         }
 
         //compute density in window of size bandWidth centered at centerOfWindow
-        while(data[endOfWindow]-dataCenter>bandWidth){
+        while(data[endOfWindow]-dataCenter>bandWidth)
+        {
             endOfWindow--;
         }
         //now window ends at endOfWindow (inclusive)
-        while(startOfWindow>0 && dataCenter-data[startOfWindow-1]<=bandWidth){
+        while(startOfWindow>0 && dataCenter-data[startOfWindow-1]<=bandWidth)
+        {
             startOfWindow--;
         }
         //now window starts at startOfWindow (inclusive)
         int currDensity=endOfWindow-startOfWindow+1;
         if(!cluster && currDensity>=maxDensity/(significance)){cluster=true;}
-        if(cluster && significance*significance*currDensity<=maxDensity && lastClusterSplit-centerOfWindow+1>=minClusterSize*data.size()){
+        if(cluster && significance*significance*currDensity<=maxDensity && lastClusterSplit-centerOfWindow+1>=minClusterSize*data.size())
+        {
             //detected cluster       
-            for(int j=lastClusterSplit; j>=centerOfWindow; j--){
+            for(int j=lastClusterSplit; j>=centerOfWindow; j--)
+            {
                  (*clusterLabel)[data[j]]=currLabel;
             }
             lastClusterSplit=centerOfWindow-1;
@@ -101,7 +113,8 @@ std::unordered_map<double,int>* oneDimensionalClustering(std::vector<double>& da
             cluster=false;
         }
         //update of iteration parameters
-        if(steps%2==0){
+        if(steps%2==0)
+        {
             centerOfWindow--;
         }
         steps++;
